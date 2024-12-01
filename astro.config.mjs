7b9855252723aa157c9eb3dc32fa5f4643754ac1
Lel Illumina <dragonbilal938@outlook.com>
2024-12-02 04:04:24 +0500

    conf

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
 // @ts-check
-// @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwind from '@astrojs/tailwind';
-
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()]
+  integrations: [tailwind(), sitemap()],
+  site: "https://snippets.nekoweb.org",
+  markdown: {
+    shikiConfig: {
+      themes: {
+        light: "everforest-light",
+        dark: "everforest-dark",
+      },
+      wrap: true,
+    },
+  },
 });
