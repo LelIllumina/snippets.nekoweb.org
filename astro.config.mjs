80218fc7411e05526e91fdd018a68f7ac83bfb7b
Lel Illumina <dragonbilal938@outlook.com>
2024-12-04 03:27:29 +0500

    add github style alerts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import { remarkAlert } from "remark-github-blockquote-alert";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-
 import selfie from "astro-selfie";
 
 // https://astro.build/config
@@ -10,6 +10,8 @@ export default defineConfig({
   integrations: [tailwind(), sitemap(), selfie()],
   site: "https://snippets.nekoweb.org",
   markdown: {
+    remarkPlugins: [remarkAlert],
+
     shikiConfig: {
       themes: {
         light: "everforest-light",
