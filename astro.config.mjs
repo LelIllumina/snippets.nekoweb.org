bd37f4ba1fa34714c7ee4ace696615921f25a9a0
Lel Illumina <dragonbilal938@outlook.com>
2024-12-04 03:22:23 +0500

    add astro-selfie

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 
+import selfie from "astro-selfie";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap()],
+  integrations: [tailwind(), sitemap(), selfie()],
   site: "https://snippets.nekoweb.org",
   markdown: {
     shikiConfig: {
