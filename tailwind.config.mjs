81a5dd3453bd49a04f75c3fc1f6151f3f9680900
Lel Illumina <dragonbilal938@outlook.com>
2024-12-06 03:52:37 +0500

    refactor: Replace css with tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,6 +4,30 @@ import typography from "@tailwindcss/typography";
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
+    fontFamily: {
+      sans: [
+        "Lexend",
+        "Inter",
+        "Roboto",
+        "Helvetica Neue",
+        "Arial Nova",
+        "Nimbus Sans",
+        "Arial",
+        "sans-serif",
+      ],
+      serif: [
+        "Instrument Serif",
+        "Times New Roman",
+        "Times",
+        "ui-serif",
+        "Georgia",
+        "serif",
+      ],
+      mono: ["Jetbrains Mono", "ui-monospace", "SFMono-Regular"],
+    },
+    colors: {
+      bg: "#1c1b22",
+    },
     extend: {},
   },
   plugins: [typography],
